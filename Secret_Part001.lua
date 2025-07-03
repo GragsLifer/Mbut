0cd4168b1b0263fa34ef1712967c09f3e2fa8811
GragsLifer <buildinto7@gmail.com>
2025-07-04 07:08:00 +0900

    Update Secret_Part001.lua

--- Secret_Part001.lua
+++ Secret_Part001.lua
@@ -11,140 +11,139 @@ obj430811.BrickColor = BrickColor.new("Medium stone grey")
 obj430811.Parent = game.Workspace
 local obj529986 = Instance.new("ModuleScript")
 obj529986.Name = "Cum"
-obj529986.Source = [[
-local cumshots = {}
-local cumgravity = 60
-
-local rng = Random.new()
-]]
-function newcumshot(cframe, vel, ignore)
-	local cum = script.cumshot:Clone()
-	local points = {}
-	local t0 = 0
-	local extravel = Vector3.zero
-
-	cum:ScaleTo(rng:NextNumber(0.03, 0.05))
-
-	local sphere = cum.Sphere
-	sphere.CFrame = cframe * CFrame.Angles(math.rad(90), 0, 0)
-	sphere.Anchored = true
-	sphere.CanCollide = false
-	sphere.CanQuery = false
-	sphere.CanTouch = false
-	sphere.Massless = true
-
-	for i = 1, 8 do
-		local v = sphere['Bone.00'..i]
-		if v then
-			--v.Orientation = Vector3.zero
-			points[#points + 1] = {v, 0, t0, v.WorldPosition, vel + extravel, false};
-			t0 += i % 2 == 0 and 1 / 60 or 0;
-			vel *= rng:NextNumber(0.925, 0.975)
-			extravel = rng:NextUnitVector() * .1
-			v.Position = Vector3.zero
-		end
-	end
-	points[#points + 1] = {sphere.Bone, 0, t0, sphere.Bone.WorldPosition, vel + extravel, false};
-	sphere.Bone.Position = Vector3.zero
-	cum.Parent = workspace.CurrentCamera
-
-	sphere.Transparency = 0.5
-	cumshots[#cumshots + 1] = {
-		cum = cum;
-		mainpart = sphere;
-		life = rng:NextNumber(5, 6);
-		points = points;
-		ignore = ignore;
-	}
-end
-
-game:GetService('RunService').Heartbeat:Connect(function(dt)
-	local j = 1
-	for i = 1, #cumshots do
-		local v = cumshots[i]
-		cumshots[i] = nil
-		if not v or not v.cum or not v.cum.Parent then
-			continue
-		end
-		
-		v.life -= dt
-		if v.life <= 0 then
-			if v.cum and not v.fading then
-				v.fading = true
-				game.TweenService:Create(v.mainpart, TweenInfo.new(4), {Transparency = 1}):Play()
-				task.delay(4, game.Destroy, v.cum)
-			end
-		end
-
-		local lastpos
-		for x = 1, #v.points do
-			local y = v.points[x]
-			if y[2] >= y[3] then
-				if not y[6] or not y[6].Parent then
-					y[6] = nil
-					local newvel = y[5] - (Vector3.yAxis * cumgravity * dt)
-					local dtvel = (newvel + y[5]) * dt / 2
-					y[5] = newvel
-					local results = workspace:Raycast(y[1].WorldPosition, dtvel, v.ignore) --v.frames < 3 and v.ignore or nil)
-					if results then
-						if not v.hit then
-							v.hit = results.Instance
-							local weld = Instance.new('WeldConstraint')
-							weld.Part0 = v.mainpart
-							weld.Part1 = results.Instance
-							weld.Parent = v.mainpart
-							v.mainpart.Anchored = false
-						end
-						y[6] = results.Instance
-						y[1].WorldCFrame = CFrame.new(results.Position, results.Position + results.Normal)
-					else
-						y[1].WorldPosition += dtvel
-					end
-				end
-			else
-				y[2] += dt
-			end
-			
-			if lastpos then
-				y[1].Transform = CFrame.fromOrientation(math.pi / 2, 0, 0)
-				y[1].WorldCFrame = CFrame.new(lastpos, y[1].WorldPosition) + (y[1].WorldPosition - lastpos)
-			elseif y[1].WorldPosition.X ~= y[4].X 
-				and y[1].WorldPosition.Y ~= y[4].Y 
-				and y[1].WorldPosition.Z ~= y[4].Z 
-			then
-				y[1].Transform = CFrame.fromOrientation(0, math.pi, 0)
-				if typeof(y[4]) == 'CFrame' then
-					y[1].WorldCFrame = y[4]
-				else
-					y[1].WorldCFrame = CFrame.new(y[4], y[1].WorldPosition) + (y[1].WorldPosition - y[4])
-				end
-			end
-			
-			if y[6] then
-				if not y[7] then
-					y[7] = y[6].CFrame:ToObjectSpace(y[1].WorldCFrame)
-				end
-				y[1].WorldCFrame = y[6].CFrame * y[7]
-			end	
-
-			y[4] = y[1].WorldPosition
-			lastpos = y[4]
-		end
-		
-		cumshots[j] = v
-		j += 1
-	end
-	j = nil
-end)
-
-return function(ignore_list)
-	local cm_ignore = RaycastParams.new()
-	cm_ignore.FilterDescendantsInstances = ignore_list or {};
-	cm_ignore.RespectCanCollide = true
-	
-	return newcumshot, cm_ignore
-...
-end]]
+obj529986.Source = "local cumshots = {}\
+local cumgravity = 60\
+\
+local rng = Random.new()\
+\
+function newcumshot(cframe, vel, ignore)\
+	local cum = script.cumshot:Clone()\
+	local points = {}\
+	local t0 = 0\
+	local extravel = Vector3.zero\
+\
+	cum:ScaleTo(rng:NextNumber(0.03, 0.05))\
+\
+	local sphere = cum.Sphere\
+	sphere.CFrame = cframe * CFrame.Angles(math.rad(90), 0, 0)\
+	sphere.Anchored = true\
+	sphere.CanCollide = false\
+	sphere.CanQuery = false\
+	sphere.CanTouch = false\
+	sphere.Massless = true\
+\
+	for i = 1, 8 do\
+		local v = sphere['Bone.00'..i]\
+		if v then\
+			--v.Orientation = Vector3.zero\
+			points[#points + 1] = {v, 0, t0, v.WorldPosition, vel + extravel, false};\
+			t0 += i % 2 == 0 and 1 / 60 or 0;\
+			vel *= rng:NextNumber(0.925, 0.975)\
+			extravel = rng:NextUnitVector() * .1\
+			v.Position = Vector3.zero\
+		end\
+	end\
+	points[#points + 1] = {sphere.Bone, 0, t0, sphere.Bone.WorldPosition, vel + extravel, false};\
+	sphere.Bone.Position = Vector3.zero\
+	cum.Parent = workspace.CurrentCamera\
+\
+	sphere.Transparency = 0.5\
+	cumshots[#cumshots + 1] = {\
+		cum = cum;\
+		mainpart = sphere;\
+		life = rng:NextNumber(5, 6);\
+		points = points;\
+		ignore = ignore;\
+	}\
+end\
+\
+game:GetService('RunService').Heartbeat:Connect(function(dt)\
+	local j = 1\
+	for i = 1, #cumshots do\
+		local v = cumshots[i]\
+		cumshots[i] = nil\
+		if not v or not v.cum or not v.cum.Parent then\
+			continue\
+		end\
+		\
+		v.life -= dt\
+		if v.life <= 0 then\
+			if v.cum and not v.fading then\
+				v.fading = true\
+				game.TweenService:Create(v.mainpart, TweenInfo.new(4), {Transparency = 1}):Play()\
+				task.delay(4, game.Destroy, v.cum)\
+			end\
+		end\
+\
+		local lastpos\
+		for x = 1, #v.points do\
+			local y = v.points[x]\
+			if y[2] >= y[3] then\
+				if not y[6] or not y[6].Parent then\
+					y[6] = nil\
+					local newvel = y[5] - (Vector3.yAxis * cumgravity * dt)\
+					local dtvel = (newvel + y[5]) * dt / 2\
+					y[5] = newvel\
+					local results = workspace:Raycast(y[1].WorldPosition, dtvel, v.ignore) --v.frames < 3 and v.ignore or nil)\
+					if results then\
+						if not v.hit then\
+							v.hit = results.Instance\
+							local weld = Instance.new('WeldConstraint')\
+							weld.Part0 = v.mainpart\
+							weld.Part1 = results.Instance\
+							weld.Parent = v.mainpart\
+							v.mainpart.Anchored = false\
+						end\
+						y[6] = results.Instance\
+						y[1].WorldCFrame = CFrame.new(results.Position, results.Position + results.Normal)\
+					else\
+						y[1].WorldPosition += dtvel\
+					end\
+				end\
+			else\
+				y[2] += dt\
+			end\
+			\
+			if lastpos then\
+				y[1].Transform = CFrame.fromOrientation(math.pi / 2, 0, 0)\
+				y[1].WorldCFrame = CFrame.new(lastpos, y[1].WorldPosition) + (y[1].WorldPosition - lastpos)\
+			elseif y[1].WorldPosition.X ~= y[4].X \
+				and y[1].WorldPosition.Y ~= y[4].Y \
+				and y[1].WorldPosition.Z ~= y[4].Z \
+			then\
+				y[1].Transform = CFrame.fromOrientation(0, math.pi, 0)\
+				if typeof(y[4]) == 'CFrame' then\
+					y[1].WorldCFrame = y[4]\
+				else\
+					y[1].WorldCFrame = CFrame.new(y[4], y[1].WorldPosition) + (y[1].WorldPosition - y[4])\
+				end\
+			end\
+			\
+			if y[6] then\
+				if not y[7] then\
+					y[7] = y[6].CFrame:ToObjectSpace(y[1].WorldCFrame)\
+				end\
+				y[1].WorldCFrame = y[6].CFrame * y[7]\
+			end	\
+\
+			y[4] = y[1].WorldPosition\
+			lastpos = y[4]\
+		end\
+		\
+		cumshots[j] = v\
+		j += 1\
+	end\
+	j = nil\
+end)\
+\
+return function(ignore_list)\
+	local cm_ignore = RaycastParams.new()\
+	cm_ignore.FilterDescendantsInstances = ignore_list or {};\
+	cm_ignore.RespectCanCollide = true\
+	\
+	return newcumshot, cm_ignore\
+end\
+"
 obj529986.Parent = game.ReplicatedStorage
     local obj420417 = Instance.new("Model")
     obj420417.Name = "cumshot"
@@ -249,5 +248,6 @@ obj529986.Parent = game.ReplicatedStorage
             obj413430.Name = "Bone.001_Original"
             obj413430.Parent = obj930887
             local obj879914 = Instance.new("CFrameValue")
-            obj879914.Name = "Bone.001_Initial"
-         
+
+    return cum
+end
